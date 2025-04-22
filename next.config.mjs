980261738aafc21c2b1ab50f5102b7fa10ea84b7
Ashrada Acharya <chillyashrada@gmail.com>
2025-04-22 07:34:43 +0545

    [Disabling swc/rust based compiling and switch to js based compiling to avoid threads not being enough]

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: false, // turn off Rust-based minifier
+  experimental: {
+    swcPlugins: [],  // make sure none are used
+    turbo: false     // disable turbo as well if used
+  }
 };
 
 export default nextConfig;
